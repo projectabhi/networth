9ffd7a826e1e1a02ba1a004a4ce44684c53697d5
projectabhi <projectabhi@gmail.com>
2025-04-13 17:13:28 +0530

    menu added on top

--- src/pages/Investments.tsx
+++ src/pages/Investments.tsx
@@ -54,6 +54,7 @@ const Investments = () => {
   };
   return (
     <>
+      <Menu />
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
           <thead className="bg-gray-50 text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
